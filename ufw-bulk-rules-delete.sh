fbd8e67faed9602341573be8c0a74e604cf5fce5
Stathis Xantinidis <spithash@hotmail.com>
2021-10-25 10:30:56 +0000

    Update ufw-bulk-rules-delete.sh

--- ufw-bulk-rules-delete.sh
+++ ufw-bulk-rules-delete.sh
@@ -1,2 +1,3 @@
- # In this example we're deleting the first 25 rules
- for i in {25..1}; do yes|ufw delete $i;done
+# In this example we're deleting the first 25 rules
+
+for i in {25..1}; do yes|ufw delete $i;done
